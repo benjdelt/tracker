4ab6980fe3765757ca85daa8051ad1223bb68600
Ben Deltenre <benjdelt@gmail.com>
2019-04-05 10:00:49 -0700

    Moved exit message in Feeder class

--- display.py
+++ display.py
@@ -8,7 +8,7 @@ import timer
 
 
 class UserInterface:
-    def __init__(self, start):
+    def __init__(self):
         self.stdscr = curses.initscr()
         curses.noecho()
         curses.cbreak()
@@ -22,8 +22,6 @@ class UserInterface:
         self.win.addstr(2, 30, "=======")
         self.win.addstr(4, 1, "Press 'p' to pause/unpause or 'q' to quit.")
 
-        self.start = start
-
     def refresh(self):
         curses.panel.update_panels()
         self.win.refresh()
@@ -34,13 +32,12 @@ class UserInterface:
         curses.curs_set(1)
         curses.echo()
         curses.endwin()
-        exit(f"Time: {timer.get_time(self.start)}\n")
 
 
 class Feeder:
     def __init__(self, start):
         self.running = False
-        self.ui = UserInterface(start)
+        self.ui = UserInterface()
         self.start = start
         self.recorded_time = 0
         self.paused = False
@@ -60,6 +57,8 @@ class Feeder:
                     if line.strip() == "q":
                         self.stop()
                         self.ui.quit_ui()
+                        exit(f"Time: {timer.get_time(self.start, self.recorded_time)}\n")
+
                         break
                     elif line.strip() == "p":
                         if not self.paused:
