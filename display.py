2f48989e0f26c6a40fbd82fac642c08564d4665a
Ben Deltenre <benjdelt@gmail.com>
2019-04-08 11:47:35 -0700

    Create and update csv files

--- display.py
+++ display.py
@@ -1,8 +1,12 @@
 import curses
 import curses.panel
 import sys
+import os
+import csv
 from select import select
-from time import sleep, time
+from collections import OrderedDict
+
+import time
 
 import timer
 
@@ -39,7 +43,8 @@ class Feeder:
         self.running = False
         self.ui = UserInterface()
         self.task = task or "None"
-        self.start = time()
+        self.first_start = time.time()
+        self.start = time.time()
         self.recorded_time = 0
         self.paused = False
 
@@ -53,7 +58,7 @@ class Feeder:
         self.running = False
         self.ui.quit_ui()
         if self.paused:
-            self.start = time() + 1
+            self.start = time.time() + 1
         task = f" in {self.task}" if self.task != "None" else "" 
         exit(f"Time{task}: {timer.get_time_string(self.start, self.recorded_time)}")
 
@@ -67,7 +72,7 @@ class Feeder:
                         break
                     elif line.strip() == "p":
                         if self.paused:
-                            self.start = time()
+                            self.start = time.time()
                             self.ui.win.addstr(14, 29, "          ")
                             self.paused = False
                         else:
@@ -77,6 +82,35 @@ class Feeder:
                             )
                             self.ui.win.addstr(14, 29, "**PAUSED**")
                             self.paused = True
+                    elif line.strip() == "s":
+                        start = time.ctime(self.first_start)
+                        finish = time.ctime()
+                        logged = timer.get_time_string(self.start, self.recorded_time)
+                        current_row = {"Start": start, "Finish": finish, "Logged": logged}
+                        lines = []
+                        try:
+                            with open(f"./{self.task}.csv", "r") as csvfile:
+                                taskreader = csv.DictReader(csvfile)
+                                headers = taskreader.fieldnames
+                                last_task = OrderedDict()
+                                for row in taskreader:
+                                    last_task = row
+                                    lines.append(row)
+                                if last_task["Start"] == start:
+                                    lines = lines[0:-1]
+                            csvfile.close()
+                            with open(f"./{self.task}.csv", "w", newline='') as csvfile:
+                                lines.append(current_row)
+                                taskwriter = csv.DictWriter(csvfile, ["Start", "Finish", "Logged"])
+                                taskwriter.writeheader()
+                                for line in lines:
+                                    taskwriter.writerow(line)
+
+                        except FileNotFoundError:
+                            with open(f"./{self.task}.csv", "w", newline='') as csvfile:
+                                taskwriter = csv.DictWriter(csvfile, ["Start", "Finish", "Logged"])
+                                taskwriter.writeheader()
+                                taskwriter.writerow(current_row)
                 if not self.paused:
                     self.ui.win.addstr(
                         9,
@@ -84,11 +118,11 @@ class Feeder:
                         timer.get_time_string(self.start, self.recorded_time)
                     )
                 self.ui.refresh()
-                sleep(0.1)
+                time.sleep(0.1)
         except KeyboardInterrupt:
             self.quit()
 
 
 if __name__ == "__main__":
-    feeder = Feeder(time())
+    feeder = Feeder(time.time())
     feeder.run()
