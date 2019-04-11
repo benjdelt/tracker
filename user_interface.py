75bc6ea45d90061ddb22986c8a876cca376e5099
Ben Deltenre <benjdelt@gmail.com>
2019-04-11 09:33:44 -0700

    Added colors

--- user_interface.py
+++ user_interface.py
@@ -8,6 +8,9 @@ class UserInterface:
         curses.noecho()
         curses.cbreak()
         curses.curs_set(0)
+        curses.start_color()
+        curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+        curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
         self.stdscr.keypad(1)
 
         self.win = curses.newwin(20, 70, 0, 0)
